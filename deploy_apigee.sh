bc682d73231836b9a504d88782b4cc344953ee4d
laihrs2 <slahiri@users.noreply.github.com>
2017-08-29 22:22:08 +0530

    added node-json-db for in-memory json handling

---
+++ deploy_apigee.sh
@@ -0,0 +1,1 @@
+apigeetool deploynodeapp -n mock -d . -m server.js -o ashwinjoshi-trial -e test -b mock -u ashwin.joshi@sapientrazorfish.com -p Sapient@123 -U
--- server.js
+++ server.js
@@ -3,6 +3,14 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var JsonDB = require('node-json-db');
+
+var db = new JsonDB("myDataBase", true, false);
+db.push("/test1","super test");
+db.push("/test2/my/test",5);
+db.push("/test3", {test:"test", json: {test:["test"]}});
+var data = db.getData("/");
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,7 +20,7 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/test1', function(req, res) {
-    res.json({ message: 'test-get' });
+    res.json( data );
 });
 
 router.post('/test2', function(req, res) {
